20b6abac289e1843aed34f5a428a207d291ad677
Abhishek Singh <abhisheksingh160102000@bbdu.ac.in>
2023-06-06 20:14:26 +0530

    updated assignment

---
+++ React/calculator/src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+
+  const [display, setDisplay] = useState('');
+
+  const handleButtonClick = (value) => {
+    setDisplay(display + value);
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(display);
+      setDisplay(result.toString());
+    } catch (error) {
+      setDisplay('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setDisplay('');
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{display}</div>
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleCalculate()}>=</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClear()}>Clear</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
